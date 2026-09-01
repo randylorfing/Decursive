a0a0f8f68add1cd0cbfeef9b2411a9b192d5b7b2
Randy Lorfing <randylorfing@gmail.com>
2026-09-01 08:53:12 -0500

    Revert "Unregister /decursive while /dcr /zd /zdecursive open options."
    This reverts commit 913561dc23f40a5762a45a254ef8acac940e03ad.

--- ZDecursive/Core.lua
+++ ZDecursive/Core.lua
@@ -38,17 +38,6 @@ function Decursive:OnInitialize()
   self:RegisterChatCommand("dcr", "OpenOptions")
   self:RegisterChatCommand("zd", "OpenOptions")
   self:RegisterChatCommand("zdecursive", "OpenOptions")
-  if self.UnregisterChatCommand then
-    self:UnregisterChatCommand("decursive")
-  end
-  SlashCmdList["ACECONSOLE_DECURSIVE"] = nil
-  _G["SLASH_ACECONSOLE_DECURSIVE1"] = nil
-  SlashCmdList["DECURSIVE"] = nil
-  _G["SLASH_DECURSIVE1"] = nil
-  if type(hash_SlashCmdList) == "table" then
-    hash_SlashCmdList["/DECURSIVE"] = nil
-    hash_SlashCmdList["/decursive"] = nil
-  end
   self:RegisterChatCommand("dcrsoullink", function(msg)
     if ns.HandleSoulLinkSlash then
       ns.HandleSoulLinkSlash(msg)
@@ -110,9 +99,6 @@ function Decursive:OnInitialize()
 end
 
 function Decursive:OnEnable()
-  if self.UnregisterChatCommand then
-    self:UnregisterChatCommand("decursive")
-  end
   self:EnsureSpecAssignments()
   self:ApplyResolvedProfile("login")
   self:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED", "OnSpecChanged")
